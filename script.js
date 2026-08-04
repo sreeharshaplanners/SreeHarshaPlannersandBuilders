41851ba1b3fdef90dd958bbaa1c0ef4820073481
sreeharshaplanners <nbsk89@gmail.com>
2026-08-04 15:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Thank you! We will contact you soon.");
+
+form.reset();
+
+});
